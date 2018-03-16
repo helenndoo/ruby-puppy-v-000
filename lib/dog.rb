3ac98bf9f86e6535b24a2f91ea61bc09c5290a3f
Helen Du <helendu.614@gmail.com>
2018-03-16 15:25:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 class Dog
   attr_accessor :name
-  @@all = []
+  @@all = [] #setting to empty array, class variable at at all
 
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def self.clear_all
+  def self.clear_all #if want to clear out our list of existing dogs
     @@all.clear
   end
 
-  def self.all
+  def self.all #class method that iterates over all of individual dogs
+    #stored in at at all array
     @@all.each {|dog| puts dog.name}
+    #puts out their name to the terminal
   end
 end
